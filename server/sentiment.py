1e0d89516fb7e6a4c991c0ef5394664b036702ab
Kade Keith <keithkade@aol.com>
2014-10-25 12:41:09 -0500

    sentiment now chunks into all of our

--- server/sentiment.py
+++ server/sentiment.py
@@ -19,12 +19,52 @@ train_features = negative_features + positive_features
 classifier = NaiveBayesClassifier.train(train_features)
 
 reviewList = []
+reviewContent = {}
 for line in open('review_KcSJUq1kwO8awZRMS6Q49g', 'r'):
     review_json = TextProcess.read_line(line)
     review_id = review_json['review_id']
+    reviewContent[review_id] = review_json['text']
 
     #print "Review %s score: %s" % (review_id, classifier.posScore(word_features(TextProcess.tokenize(review_json))))
     reviewList.append((review_id, classifier.posScore(word_features(TextProcess.tokenize(review_json)))))
 
 reviewList = sorted(reviewList, key=lambda count: count[1], reverse=True)
-#print review_json[str(reviewList[0][0])]['text']
+
+nchunks = 9
+chunksize,remainder = divmod(len(reviewList),nchunks)
+sizes = [chunksize]*nchunks
+if remainder:
+    sizes[-remainder:] = [chunksize+1 for x in xrange(remainder)]
+idx = 0
+result = []
+for s in sizes:
+    result.append(reviewList[idx:idx+s])
+    idx += s
+
+for i in range(0, 9):
+    curChunk = result[i]
+    #for j in curChunk:
+    for j in range (0, len(curChunk)):
+        jM = list(curChunk[j])
+        if i == 0:
+            jM[1] = 5
+        if i == 1:
+            jM[1] = 4.5
+        if i == 2:
+            jM[1] = 4
+        if i == 3:
+            jM[1] = 3.5
+        if i == 4:
+            jM[1] = 3
+        if i == 5:
+            jM[1] = 2.5
+        if i == 6:
+            jM[1] = 2
+        if i == 7:
+            jM[1] = 1.5
+        if i == 8:
+            jM[1] = 1
+        result[i][j] = jM
+
+print result
+#print reviewContent[str(result[0][0][0])]
