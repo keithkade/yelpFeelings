a51e6d123b3d7e6cb4beeda6cc75bf3334e6e343
John Goen <jtgoen@gmail.com>
2014-11-30 16:52:36 -0600

    Updated server to support displaying of most controversial reviews.

--- server/server.py
+++ server/server.py
@@ -53,6 +53,11 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write(
                 json.dumps(
                     sorted(city_businesses, key=lambda business: business['stars'])[:10]))
+        elif sort == "Most Controversial":
+            print "Most Controversial"
+            self.wfile.write(
+                json.dumps(
+                    sorted(city_businesses, key=lambda business: abs(business['stars'] - business['sentiment']))[:10]))
 
         return
 
